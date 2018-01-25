11054c26ab99171ac8354bae6f68a3d63eccb8a4
mawen12 <1181963012mw@gmail.com>
2018-01-25 23:02:10 +0800

    print"HelloWorld"

--- src/tmall/dao/PropertyDAO.java
+++ src/tmall/dao/PropertyDAO.java
@@ -4,20 +4,23 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import tmall.bean.Category;
 import tmall.bean.Property;
 import tmall.util.DBUtil;
 
 public class PropertyDAO {
 	
-	public int getTotal() {
+	public int getTotal(int cid) {
 		int total = 0;
-		String sql = "select count(*) from property";
 		try(Connection c = DBUtil.getConnection();
-			PreparedStatement ps = c.prepareStatement(sql);)
+			Statement s = c.createStatement();)
 		{
-			ResultSet rs = ps.executeQuery();
+			String sql = "select count(*) from property where cid = "+cid ;
+			ResultSet rs = s.executeQuery(sql);
 			while(rs.next()) {
 				total = rs.getInt(1);
 			}
@@ -53,6 +56,10 @@ public class PropertyDAO {
 		try(Connection c = DBUtil.getConnection();
 			PreparedStatement ps = c.prepareStatement(sql);)
 		{
+			ps.setInt(1, bean.getCategory().getId());
+			ps.setString(2, bean.getName());
+			ps.setInt(3, bean.getId());
+			ps.execute();
 			
 		}catch(SQLException e) {
 			e.printStackTrace();
@@ -61,18 +68,72 @@ public class PropertyDAO {
 	}
 	
 	public void delete(int id) {
-		
+		try(Connection c = DBUtil.getConnection();
+			Statement s = c.createStatement();)
+		{
+			String sql = "delete from property where id = "+ id;
+			s.execute(sql);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public Property get(int id) {
-		
+		Property bean = null;
+		try(Connection c = DBUtil.getConnection();
+			Statement s = c.createStatement();)
+		{
+			String sql = "select * from property where id = "+ id;
+			ResultSet rs = s.executeQuery(sql);
+			if(rs.next()) {
+				bean = new Property();
+				int cid = rs.getInt(2);
+				String name = rs.getString(3);
+				Category category = new CategoryDAO().get(cid);
+				
+				bean.setCategory(category);
+				bean.setName(name);
+				bean.setId(id);
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return bean;
 	}
 	
-	public List<Property> list(){
-		
+	public List<Property> list(int cid){
+		return list(cid,0, Short.MAX_VALUE);
 	}
 	
-	public List<Property> list(int start, int count){
+	public List<Property> list(int cid, int start, int count){
+		List<Property> beans = new ArrayList<Property>();
+		Property bean = null;
+		String sql = "select * from property where cid = ? order by int desc limit ?, ?";
+		try(Connection c = DBUtil.getConnection();
+			PreparedStatement ps = c.prepareStatement(sql);)
+		{
+			ps.setInt(1, cid);
+			ps.setInt(2, start);
+			ps.setInt(3, count);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				bean = new Property();
+				int id = rs.getInt(1);
+				String name = rs.getString("name");
+				Category category  = new CategoryDAO().get(cid);
+				
+				bean.setId(id);
+				bean.setCategory(category);
+				bean.setName(name);
+				beans.add(bean);
+				
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return beans;
 		
 	}
 }
